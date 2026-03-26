c6b2af469e0482977a63d739b716c51454d87076
jaRamirezAg <juananr99@gmail.com>
2026-03-26 20:06:13 +0100

    Fix grep string in finder-test

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -51,7 +51,7 @@ echo "${OUTPUTSTRING}" > /tmp/assignment4-result.txt
 rm -rf /tmp/aeld-data
 
 # Verificar si el resultado coincide con lo esperado
-if echo "${OUTPUTSTRING}" | grep -q "The number of files are ${NUMFILES}"; then
+if echo "${OUTPUTSTRING}" | grep -e "The number of files are ${NUMFILES}" | grep -q "the number of matching lines are"; then
     echo "success"
     exit 0
 else
